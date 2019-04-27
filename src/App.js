416ca114d07e0b76b7ddaf2d33cc6f748a9dd6e8
xisco <gistextra@gmail.com>
2019-04-27 16:21:35 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -48,6 +48,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(console.log)
+  }
+
   calculateFaceLocation = (data) => {
     const faceCordinate = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
